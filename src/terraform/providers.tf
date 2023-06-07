7d96a6d46f2940fcb3d7ba4d814940534dc9937a
Claudio Canales <klaudioz@gmail.com>
2023-06-07 20:39:16 -0300

    Add everything. Check if it works

---
+++ src/terraform/providers.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.68.0"
+    }
+  }
+}
+
+provider "google" {
+  project = "tf-gcp-gha"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
